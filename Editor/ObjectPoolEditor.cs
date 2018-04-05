8503832866045228342b19e916ed5d74c73bae15
dmitrii <dvzyrianov@fullsail.edu>
2018-04-05 13:38:33 -0400

    Init Commit

---
+++ Editor/ObjectPoolEditor.cs
@@ -0,0 +1,116 @@
+﻿/*  ╔═════════════════════════════╡  DinoTank 2018 ╞════════════════════════════╗            
+    ║ Authors:  Dmitrii Roets                       Email:    roetsd@icloud.com ║
+    ╟───────────────────────────────────────────────────────────────────────────╢░ 
+    ║ Purpose:  Extends/overrides the functionality of ObjectPool script        ║░
+    ║ Usage:    This script must be in Editor folder                            ║░
+    ╚═══════════════════════════════════════════════════════════════════════════╝░
+       ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectPool))]
+public class ObjectPoolEditor : Editor
+{
+    ObjectPool objectPool;
+    Color defBackgroundColor;
+
+    private bool showInheritedvars = false;
+    float labelWidth;
+    float fieldWidth;
+
+    public override void OnInspectorGUI()
+    {
+        objectPool = (ObjectPool)target;
+        defBackgroundColor = GUI.backgroundColor;
+        labelWidth = EditorGUIUtility.labelWidth;
+        fieldWidth = EditorGUIUtility.fieldWidth;
+
+        // ----------------------Begin Logic ---------------------------
+        objectPool.debugMode = EditorGUILayout.Toggle(new GUIContent("Debug Mode", "Log names and usage of objects?"), objectPool.debugMode);
+     
+        Heading1("Preloaded Objects", Color.yellow);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("prewarmedObjects"), new GUIContent("Prewarmed Objects", "List of objects to create at load"), true);
+        EditorGUILayout.BeginVertical("box");
+        Heading1("Current Pooled Objects", Color.cyan);   
+        for (int i = 0; i < objectPool.objects.Count; i++)
+        {
+            ShowObjectEntry(i, objectPool.objects[i], objectPool.objectsNames[i]);
+        }    
+        EditorGUILayout.EndVertical();
+
+        // ----------------------End Logic ---------------------------
+        EditorGUIUtility.labelWidth = labelWidth;
+        EditorGUIUtility.fieldWidth = fieldWidth;
+        EditorGUILayout.Space();
+        GUI.color = Color.magenta;
+        showInheritedvars = EditorGUILayout.Toggle("Show Full Script", showInheritedvars);
+        EditorGUILayout.LabelField("__________________________________________________");
+        GUI.color = defBackgroundColor;
+
+        if (showInheritedvars)
+            DrawDefaultInspector();
+
+        EditorUtility.SetDirty(target);
+        serializedObject.Update();
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    void ShowObjectEntry(int _index, GameObject _object, string _corespondingName)
+    {
+        if (!_object)
+        {
+            EditorGUIUtility.labelWidth = 200;
+            GUI.color = CustomColors.LightRED;
+            EditorGUILayout.BeginHorizontal("box");
+
+            if (objectPool.debugMode)
+                EditorGUILayout.LabelField(objectPool.objectsNames[_index]);
+
+            _object = EditorGUILayout.ObjectField(_index.ToString(), _object, typeof(GameObject), true) as GameObject;
+
+            EditorGUILayout.EndHorizontal();
+            return;
+        }
+        else
+        {
+            EditorGUIUtility.labelWidth = 50;
+            if (!_object.activeInHierarchy) // object ready to be recycled
+            {
+                GUI.color = defBackgroundColor;
+                EditorGUILayout.BeginHorizontal("button");
+                EditorGUILayout.ObjectField("READY ", _object, typeof(GameObject), true);
+
+                if (objectPool.debugMode && _index < objectPool.used.Count)
+                {
+                    EditorGUILayout.LabelField(objectPool.objectsNames[_index]);
+                    EditorGUILayout.LabelField("Used: ", objectPool.used[_index].ToString());
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            else // object in use
+            {
+                GUI.color = Color.gray;
+                EditorGUILayout.BeginHorizontal("button");
+                EditorGUILayout.ObjectField("IN USE ", _object, typeof(GameObject), true);
+                EditorGUILayout.EndHorizontal();
+            }
+
+        }
+
+    }
+
+    void Heading1(string _heading, Color _color, bool _AddSpaces = false)
+    {
+        if (_AddSpaces) EditorGUILayout.Space();
+        GUI.color = _color;
+        EditorGUILayout.HelpBox(_heading, MessageType.None);
+        GUI.color = defBackgroundColor;
+        if (_AddSpaces) EditorGUILayout.Space();
+    }
+
+
+}
---
+++ ObjectPool.cs
@@ -0,0 +1,206 @@
+﻿/*  ╔════════════════════════════════╡  DinoTank 2018 ╞══════════════════════════════╗            
+    ║ Authors:  Dmitrii Roets                       Email:    roetsd@icloud.com      ║
+    ╟────────────────────────────────────────────────────────────────────────────────╢░ 
+    ║ Purpose: This system is designed to replace standard Instantiation             ║░
+    ║          procedure and Destroy. The objects created via this system are        ║░
+    ║          recycled. Saves memory and performance.                               ║░
+    ║ Usage:   Use ObjectPool.InstantiateGameObject(somePrefab, position, rotation)  ║░
+    ║          and  ObjectPool.DestroyGameObject(someObject) to make objects you want║░                                                               
+    ╚════════════════════════════════════════════════════════════════════════════════╝░
+       ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPool : MonoBehaviour {
+  
+    public bool prewarmObjectPool = true;
+    public List<GameObject> prewarmedObjects = new List<GameObject>();
+    public List<GameObject> objects = new List<GameObject>(); 
+
+    // Debugging 
+    public bool debugMode = true;
+    public List<string> objectsNames = new List<string>(); 
+    public List<int> used = new List<int>(); 
+
+    // Singleton
+    private static ObjectPool objectPool;
+    public static ObjectPool instance
+    {
+        get
+        {
+            if (!objectPool)
+            {
+                objectPool = FindObjectOfType(typeof(ObjectPool)) as ObjectPool;
+                if (!objectPool)
+                {
+                    Debug.Log("Could not find EventManager in scene, will make one");
+                    GameObject manager = Instantiate(Resources.Load("Managers/ObjectPool", typeof(GameObject)) as GameObject);
+                    if (manager)
+                        objectPool = manager.GetComponent<ObjectPool>();
+                    else
+                    {
+                        Debug.Log("<Color=Maroon><b>ObjectPool</b></Color>: <Color=Yellow>No Prefab</Color> detected, will make one for this run (good idea to make a prefab in resources)");
+                        GameObject newObjectPool = new GameObject("TempObjectPool");
+                        newObjectPool.AddComponent<ObjectPool>();
+                        objectPool = newObjectPool.GetComponent<ObjectPool>();
+                    }
+                }
+            }
+            return objectPool;
+        }
+    }
+
+   
+    void Awake()
+    {
+        if (objectPool == null)
+        {
+            objectPool = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        Init();
+        transform.SetParent(null);
+    }
+    
+
+    public void Init()
+    {
+        if (!prewarmObjectPool)
+            objects.Clear();
+        else
+        {
+            foreach (GameObject item in prewarmedObjects)
+            {
+                if (item)
+                {
+                    GameObject newObject = Instantiate(item);
+                    newObject.name = item.name;
+                    instance.objects.Add(newObject);
+
+                    if (instance.debugMode)
+                    {
+                        instance.objectsNames.Add(newObject.name + "_PRE");
+                        instance.used.Add(0);
+                    }
+                    newObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public static GameObject InstantiateGameObject(GameObject _object, bool _activate = true)
+    {
+        if (_object == null)
+        {
+            Debug.Log("<Color=Maroon><b>DinoError</b></Color>: <Color=Yellow>ObjectPool</Color> was instructed to make null object");
+            return null;
+        }
+        for (int i = 0; i < instance.objects.Count; i++)
+        {
+            if (instance.objects[i])
+            {
+                if (!instance.objects[i].activeInHierarchy && instance.objects[i].name.Contains(_object.name))
+                {
+                    if (instance.debugMode)
+                    {
+                        instance.used[i]++;
+                    }
+                    return instance.objects[i];
+                }
+            }
+            else
+            {
+                if (instance.debugMode)
+                {
+                    Debug.LogWarning("DinoWarning : ObjectPool has null for " + instance.objectsNames[i] + " object in list when called to create " + _object.name);
+                }
+            }
+        }
+         
+        GameObject newObject = Instantiate(_object);
+        newObject.name = _object.name;
+        instance.objects.Add(newObject);
+
+        if (instance.debugMode)
+        {
+            instance.objectsNames.Add(newObject.name + "_DYN");
+            instance.used.Add(1);
+        }
+
+        newObject.SetActive(_activate);
+
+        return newObject;
+    }
+    public static GameObject InstantiateGameObject(GameObject _object, Vector3 _position, Quaternion _rotation)
+    {
+      if(_object == null)
+        {
+            Debug.LogError("<Color=Maroon><b>ObjectPool</b></Color>: <Color=Yellow> null object </Color> fed to ObjectPool");
+            return null;
+        }
+
+        for (int i = 0; i < instance.objects.Count; i++)
+        {
+            if (instance.objects[i])
+            {
+                if (instance.objects[i].name.Contains(_object.name) && !instance.objects[i].activeInHierarchy)
+                {
+                    instance.objects[i].transform.position = _position;
+                    instance.objects[i].transform.rotation = _rotation;
+                    instance.objects[i].SetActive(true);
+
+                    if (instance.debugMode)
+                    {
+                        instance.used[i]++;
+                    }
+
+                    return instance.objects[i];
+                }
+            }
+        }
+
+        GameObject newObject = Instantiate(_object);
+        newObject.name = _object.name;
+        newObject.transform.position = _position;
+        newObject.transform.rotation = _rotation;
+        newObject.SetActive(true);
+        instance.objects.Add(newObject);
+
+        if (instance.debugMode)
+        {
+            instance.objectsNames.Add(newObject.name + "_DYN");
+            instance.used.Add(1);
+        }
+        return newObject;
+    }
+
+    public static bool Contains(GameObject _object)
+    {
+        return instance.objects.Contains(_object);
+    }
+    public static void DestroyGameObject(GameObject _obj)
+    {
+        for (int i = 0; i < instance.objects.Count; i++)
+        {
+            if (!instance.objects[i])
+            {
+            }
+            else
+            {
+                if (instance.objects[i] == _obj)
+                {
+                    _obj.transform.SetParent(null);
+                    _obj.SetActive(false);
+                    return;
+                }
+            }
+        }
+        Destroy(_obj); 
+    }
+}
